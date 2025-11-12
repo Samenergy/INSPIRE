909d7683afd566a4e1ca858f0c573bfdcd7af068
Samuel Dushime <sdushime38@gmail.com>
2025-11-12 20:17:54 +0200

    d

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -189,11 +189,12 @@ app = FastAPI(
 )
 
 origins = [
-    "https://inspire.software/",
+    "https://inspire.software",
     "http://localhost",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
     "http://localhost:3000",
+
 ]
 
 app.add_middleware(
